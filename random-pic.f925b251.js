60bf291ba6c607c4b8521a66669ad8ce6633ff5e
HungI <hominthesky@gmail.com>
2019-01-09 02:53:35 +0800

    add pages

---
+++ random-pic.f925b251.js
@@ -0,0 +1,12 @@
+parcelRequire=function(e,r,n,t){var i="function"==typeof parcelRequire&&parcelRequire,o="function"==typeof require&&require;function u(n,t){if(!r[n]){if(!e[n]){var f="function"==typeof parcelRequire&&parcelRequire;if(!t&&f)return f(n,!0);if(i)return i(n,!0);if(o&&"string"==typeof n)return o(n);var c=new Error("Cannot find module '"+n+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[n][1][r]||r},p.cache={};var l=r[n]=new u.Module(n);e[n][0].call(l.exports,p,l,l.exports,this)}return r[n].exports;function p(e){return u(p.resolve(e))}}u.isParcelRequire=!0,u.Module=function(e){this.id=e,this.bundle=u,this.exports={}},u.modules=e,u.cache=r,u.parent=i,u.register=function(r,n){e[r]=[function(e,r){r.exports=n},{}]};for(var f=0;f<n.length;f++)u(n[f]);if(n.length){var c=u(n[n.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=c:"function"==typeof define&&define.amd?define(function(){return c}):t&&(this[t]=c)}return u}({"tsXD":[function(require,module,exports) {
+module.exports="/icon-1.a6c23193.png";
+},{}],"6YtJ":[function(require,module,exports) {
+module.exports="/icon-2.9d6f3ea4.png";
+},{}],"NWGB":[function(require,module,exports) {
+module.exports="/icon-3.99e627a2.png";
+},{}],"nBx9":[function(require,module,exports) {
+module.exports="/icon-4.d07b5ffb.png";
+},{}],"enof":[function(require,module,exports) {
+var a=Math.random(),e=4,s=Math.round(a*(e-1))+1;images=[],images[1]=require("../assets/random/icon-1.png"),images[2]=require("../assets/random/icon-2.png"),images[3]=require("../assets/random/icon-3.png"),images[4]=require("../assets/random/icon-4.png"),window.image=images[s];
+},{"../assets/random/icon-1.png":"tsXD","../assets/random/icon-2.png":"6YtJ","../assets/random/icon-3.png":"NWGB","../assets/random/icon-4.png":"nBx9"}]},{},["enof"], null)
+//# sourceMappingURL=/random-pic.c4647034.map
